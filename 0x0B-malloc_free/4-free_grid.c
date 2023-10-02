d315e003cb8f0eeae96a5beb8569dcb6ef0b1a65
livechimney <adrianmuriuki1366@gmail.com>
2023-10-02 14:25:14 -0700

    last commit for task 4

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -1,21 +1,22 @@
+#include <stdio.h>
 #include <stdlib.h>
 #include "main.h"
-#include <stdio.h>
 /**
- *free_grid-free 2D grid
- *which were created by alloc_grind
- *@grid: 2d grid
- *@height: height dimension of grid
- *description: frees memory of grid
- *compile alloc_grind.c
+ * free_grid - frees 2d array
+ * @grid: 2d grid
+ * @height: height dimension of grid
+ * Description: frees memory of grid
+ * Return: nothing
+ *
  */
 void free_grid(int **grid, int height)
+{
+	int i;
 
-{
-if (grid != NULL && height != 0)
-{
-for (; height >= 0; height--)
-free(grid);
-free(grid[height]);
+	for (i = 0; i < height; i++)
+	{
+		free(grid[i]);
+	}
+	free(grid);
 }
-}
+

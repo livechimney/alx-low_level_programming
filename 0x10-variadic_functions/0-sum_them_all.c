de66faeeb5548534c12edc7b09349a38f891237e
livechimney <adrianmuriuki1366@gmail.com>
2023-10-12 04:31:47 -0700

    initial 0x10. C - Variadic functions commit

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,32 @@
+#include "variadic_functions.h"
+/**
+ * sum_them_all - sum all paremeter passed to function
+ * @n: number of argumnet to be passed to sum
+ * Return: sum
+ */
+
+int sum_them_all(const unsigned int n, ...)
+{
+	unsigned int sum = 0;
+	unsigned int i;
+
+	va_list ptr;
+
+	va_start(ptr, n);
+
+	for (i = 0; i < n; i++)
+	{
+	if (n == 0)
+	{
+	return (0);
+	}	
+	else
+{
+sum += va_arg(ptr, const unsigned int);
+}
+
+}
+va_end(ptr);
+return (sum);
+
+}
---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,15 @@
+#ifndef VARIADIC_FUNCTION
+#define VARIADIC_FUNCTION
+
+#include <stdarg.h>
+#include <stddef.h>
+#include <stdio.h>
+
+
+
+int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+
+#endif

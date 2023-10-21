7b59b33ea5d071fe05d7a945fa21383b4a58e301
livechimney <adrianmuriuki1366@gmail.com>
2023-10-21 03:30:57 -0700

    dog 1 commit

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -2,20 +2,20 @@
 #include "dog.h"
 /**
  * init_dog - new dog struct with variable it to initialize
- *intialized
- * @d: pointer to the struct of the new dog
- * @name: of dog
- * @age:of dog
- * @owner: of dog
+ *
+ * @d: The dog to be initialized
+ * @name: The name of dog
+ * @age:The age of dog
+ * @owner: The owner of the dog
  */
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 
-if (d == NULL)
-d = malloc(sizeof(struct dog));
+if (d != NULL)
+{
 d->name = name;
 d->age = age;
 d->owner = owner;
-
+}
 }

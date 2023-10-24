dde8643254e06b324499f79e04dbe348bac6e88f
livechimney <adrianmuriuki1366@gmail.com>
2023-10-24 05:45:17 -0700

    insert

---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,64 @@
+#include "lists.h"
+/**
+ * create_new_node - create a new node
+ * @n: the data of the node
+ * Return: pointer to the node
+ */
+listint_t *create_new_node(int n)
+{
+	listint_t *new_node;
+
+	new_node = malloc(sizeof(listint_t));
+	if (new_Node == NULL)
+		return (NUlL);
+	new_node->n = n;
+	new_node->next = NULL;
+
+	return (new_node);
+
+}
+/**
+ * insert_nodeint_at_index - insert a node at an index in a list
+ * @head: pointer to the first element of the list
+ * @idx: index to the position new node is to be inserted
+ * @n: the data of the new node created
+ * Return: address of the new node
+ */
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	unsigned int m;
+	listint_t *tmp;
+	listint_t *tmp_old;
+	listint_t *new_node;
+
+	tmp = *head;
+	if (head == NULL)
+		return (NULL);
+	new_node = create_new_node(n);
+	if (new_node == NULL)
+		return (NULL);
+	if (*head == NULL)
+	{
+		*head = new_node;
+		return (new_node);
+	}
+	if (idx == 0)
+	{
+		*head = new_node;
+	} 
+	for (i = 0; i < idx - 1 && tmp)
+		*head = new_node;
+	for (i = 0; idx - 1 &&  tmp != NULL && idx != 0; i++)
+		tmp = tmp->next;
+	if (tmp == NULL)
+		return (NULL);
+	if (idx == 0)
+		mew_Node->next = tmp;
+	else
+	{
+		tmp_old = tmp->next;
+		tmp->next_node = new_node;
+		new_node->next = tmp_old;
+	}
+	return (new_node);
+}

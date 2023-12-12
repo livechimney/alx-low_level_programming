75a8fbdca46af54b950fc170b21dcfe7874579ec
livechimney <adrianmuriuki1366@gmail.com>
2023-12-12 05:22:12 -0800

    Create a dynamic library that contains C functions that can be called from Python. See example for more detail.

---
+++ 0x18-dynamic_libraries/mul.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * mul -multilply two inegers
+ * @a: first int
+ * @b: second int
+ * return: multiplication
+ */
+int mul(int a, int b)
+{
+	return(a * b);
+}
